40a8e114473fd24a6527c8b95d251c35557e3553
Firebrose90 <ambrosewong06@gmail.com>
2018-03-17 14:08:21 +0800

    Phaser

--- Phaser/app.js
+++ Phaser/app.js
@@ -9,7 +9,7 @@ function preload(){
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
   game.load.spritesheet('baddie', 'assets/baddie.png', 32, 32);
-  game.load.spritesheet('firstaid','asset/firstaid.png');
+  game.load.spritesheet('firstaid','assets/firstaid.png');
 }
 
 function create(){
